d47f433adc8d805ba6acdc6c167e7ff6155b77a2
Ricardo <francoliricardo@gmail.com>
2021-02-24 16:25:59 +0100

    change button behaviour

--- js/app.js
+++ js/app.js
@@ -61,15 +61,4 @@ const swiper = new Swiper('.swiper-container', {
 	breakpoints: {
 		1200: { slidesPerView: 5 },
 	},
-
-	// If we need pagination
-	// pagination: {
-	// 	el: '.swiper-pagination',
-	// },
-
-	// Navigation arrows
-	// navigation: {
-	// 	nextEl: '.swiper-button-next',
-	// 	prevEl: '.swiper-button-prev',
-	// },
 });
